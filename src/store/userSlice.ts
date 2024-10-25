e5a1f37645f9e8026ad8c86bd9c136f4191bfec7
Asdian <asdiank7@gmail.com>
2024-10-25 17:19:25 +0500

    create folder store at src folder. install @reduxjs/toolkit react-redux. In store create store.ts and userSlice.ts

---
+++ src/store/userSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: null
+}
+
+const userSlice = createSlice({
+    name: "userSlice",
+    initialState,
+    reducers:
+})
